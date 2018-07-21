dfe0efcbd87653c573c4a5772e4999da1f6199f0
Michael <mkeating2225@gmail.com>
2018-07-21 17:59:03 -0400

    add sass support, basic nav and styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import Link from "gatsby-link";
 
+import './index.scss';
+
+const ListLink = props => (
+		<li>
+			<Link to={props.to}>
+				{props.children}
+			</Link>
+		</li>
+	);
+
 export default({ children }) => (
-		<div>
-			<nav>
+		<div className="main">
+			<nav className="nav">
 				
-				<Link to="/">Home</Link>
-				<Link to="/blog/">Blog</Link>
-				<Link to="/me/">Me</Link>
-				<Link to="/work/">Work</Link>
+				<Link to="/"><h1>Home</h1></Link>
+
+				<div className="nav-items">
+					<ul className="nav-list">
+						<ListLink to="/blog/">Blog</ListLink>
+						<ListLink to="/me/">Me</ListLink>
+						<ListLink to="/work/">Work</ListLink>
+					</ul>
+				</div>
+				
 
 			</nav>
+			
+
 			{children()}
+
+			<div className="mobile-nav-button">
+				<div className="bar1"></div>
+				<div className="bar2"></div>
+				<div className="bar3"></div>
+			</div>
 
 		</div>
 	);
