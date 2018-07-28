e64c939ad4307282dfa31b95b05d6a07a1789359
Michael <mkeating2225@gmail.com>
2018-07-28 18:45:17 -0400

    add blog index page

---
+++ src/pages/blog.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import Link from 'gatsby-link';
+
+const IndexPage = ({data}) => {
+  const {edges: posts} = data.allMarkdownRemark;
+  return (
+    <div>
+      {posts.map (({node: post}) => {
+        const {frontmatter} = post;
+        return (
+          <div>
+            <h2>
+              <Link to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
+            </h2>
+            <p>{frontmatter.date}</p>
+            <p>{frontmatter.excerpt}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark( filter: { frontmatter: { path: { regex: "/\/blog\/(.*)/" } } } sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
