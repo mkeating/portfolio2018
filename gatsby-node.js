2d66348fee5782ea9f40627404b94e998fbb0e8a
Michael <mkeating2225@gmail.com>
2018-08-04 12:10:23 -0400

    remove material ui to fix netlify deployment issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require("path");
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`);
+  const BlogPostTemplate = path.resolve(`src/templates/blogTemplate.js`);
 
   return graphql(`
     {
@@ -28,7 +28,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
         path: node.frontmatter.path,
-        component: blogPostTemplate,
+        component: BlogPostTemplate,
         context: {}, // additional data can be passed via context
       });
     });
