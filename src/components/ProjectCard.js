6b236b58ad5647932c42bb4b3b88c1558a85983e
Michael <mkeating2225@gmail.com>
2018-09-23 11:42:26 -0400

    fix case error in netlify build

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link'
+import { withStyles } from 'material-ui/styles';
+import Card from 'material-ui/Card';
+import CardActions from 'material-ui/Card';
+import CardContent from 'material-ui/Card';
+import CardMedia from 'material-ui/Card';
+import Button from 'material-ui/Button';
+
+/*
+import Card from "react-md/lib/Cards/Card";
+import Media, { MediaOverlay } from "react-md/lib/Media";
+*/
+
+
+const styles = {
+  card: {
+    margin: 20,
+  },
+  media: {
+    height: 300,
+  },
+};
+
+/**/
+
+function ProjectCard(props) {
+
+  const { classes } = props
+  return(
+    <div>
+        
+       <Card className={classes.card}>
+          <CardMedia className={classes.media}
+            title = "wahatever"
+            classes = {{root: 'myProjClass'}}
+            style = {{backgroundImage: `url(/images/pulp-bg.jpg)`, backgroundSize: 'cover'}}
+
+          /> 
+
+
+          <CardContent
+            style = {{padding: '20px'}}
+          >  
+            <h2>{props.data.title}</h2>
+            <p>{props.data.date}</p>
+            <p>{props.data.description}</p>
+            Built With:
+            {
+              props.data.builtWith.map((item) => {
+                return <div>{item}</div>
+              })
+            }  
+          </CardContent>
+      </Card>
+      
+    </div>
+    )
+}
+
+ProjectCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProjectCard);
