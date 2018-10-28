f2f2ace5e04663f256da18e640b581ffddbff2c3
Michael <mkeating2225@gmail.com>
2018-10-28 16:40:02 -0400

    placeholder content for pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,11 @@ import ProjectCard from '../components/ProjectCard'
 
 const IndexPage = () => {
 
-  return(<div> Hello</div>)  
+  return(<div> 
+        <h1 className="home-header">Hello.</h1>
+
+        <p>My name is Mike and I'm a JavaScript and PHP developer working around Boston. Check out some of <a href="/projects/">my work</a>, read what I've <a href="/blog/">written</a>, or send me an email: mkeating2225@gmail.com</p>
+      </div>)  
 
   }
 
